0a21714fe41b97f88e5f611705939161758591c3
Naillin <kamilasd@yandex.ru>
2024-11-19 05:31:38 +0500

    add cors

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import sqlite3
 
 app = Flask(__name__)
+
+# Разрешаем CORS для всех доменов
+CORS(app)
 
 # Добавление нового топика
 @app.route('/add_topic', methods=['POST'])
